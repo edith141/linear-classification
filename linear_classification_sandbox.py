de4c886e1b6cac90e562568ff3867caa5fb8a1b7
Ishaan <53191150+edith141@users.noreply.github.com>
2022-06-16 22:54:13 +0530

    update

--- linear_classification_sandbox.py
+++ linear_classification_sandbox.py
@@ -2,22 +2,19 @@ from math import exp
 from random import shuffle
 # prepare dataset
 def getDataSet():
-    # col1 = []
-    # col2 = []
-    # col3 = []
+    # get the dataset from the file. transform it into a suitable form.
+    # then shuffle it and return.
     dataset = []
     with open("examples/earthquake-clean.data.txt", 'r') as f:
         for line in f:
             first, sec, thrd = line.split(",")
-            # col1.append(first)
-            # col2.append(sec)
-            # col3.append(thrd)
             dataset.append([float(first), float(sec), float(thrd)])
     shuffle(dataset)
     return dataset
+    # dataset of an OR gate.
     # return [[1,1,1],[1,0,1],[0,0,0],[0,1,1],[1,1,1],[1,0,1],[0,0,0],[0,1,1],[1,1,1],[1,0,1],[0,0,0],[0,1,1]]
  
-#  spilt data set into train, test
+#  spilt data set into train, test acc to the given ratio.
 def trainTestSplit2(dataset, ratio):
     elems = len(dataset)
     middle = int(elems * ratio)
@@ -27,50 +24,62 @@ def trainTestSplit2(dataset, ratio):
     print("\n")
     return dataset[:middle], dataset[middle:]
 
+def get_avg_from_arr(arr):
+    # returns avg value from an array.
+    sum = 0
+    N = len(arr)
+    for elem in arr:
+        sum+=elem
+    return (sum/N)
+
 def getAccuracy(act, pred):
+    # get the accuracy of the model.
+    # simply compare pred and actual values.
     corr = 0
     for i in range(len(act)):
         if act[i] == pred[i]:
             corr+=1
     return (corr / float(len(act)) * 100)
 
-# func Predict -> Make a prediction of y (yHat) with the given coefficients
+# func Predict -> Make a prediction of y (yHat) with the given coeffficients
 # yHat = 1.0 / (1.0 + e^(-(b0 + b1 * x1))) 
 # b0 -> bias/intercept
-# b1 -> coeff. for current (single) value of x
-def predict(row, coefficients): 
-	yhat = coefficients[0]
+# b1 -> coefff. for current (single) value of x
+# yHat is nothing but the h(x) as given in the readme.
+def predict(row, coeffficients): 
+	yhat = coeffficients[0]
 	for i in range(len(row)-1):
-		yhat += coefficients[i + 1] * row[i]
+		yhat += coeffficients[i + 1] * row[i]
     # the sigmoid(x) function -> 1 / (1 + e^(x-1))
 	return 1.0 / (1.0 + exp(-yhat))
  
-# Estimate logistic regression coefficients using stochastic gradient descent
-def coefficients_sgd(train, l_rate, n_epoch):
-	coef = [0.0 for i in range(len(train[0]))]
+# Estimate logistic regression coeffficients using stochastic gradient descent
+def coefficientsSGD(train, LR, n_epoch):
+	coeff = [0.0 for i in range(len(train[0]))]
 	for epoch in range(n_epoch):
-		sum_error = 0
+		summError = 0
 		for row in train:
-			yhat = predict(row, coef)
+			yhat = predict(row, coeff)
 			error = row[-1] - yhat
             # error sum squared for logging
-			sum_error += error**2
-            # coeff one updating here - the y-intercept or the bias
+			summError += error**2
+            # coefff one updating here - the y-intercept or the bias
             # this is in the outer loop as this doesn't depend on individual values of x input.
-			coef[0] = coef[0] + l_rate * error * yhat * (1.0 - yhat)
+			coeff[0] = coeff[0] + LR * error * yhat * (1.0 - yhat)
 			for i in range(len(row)-1):
-                # this is the weight coeff. to the input x and is hence dependent on each value of x.
+                # this is the weight coefff. to the input x and is hence dependent on each value of x.
                 # so it is updated in each iteration..
-				coef[i + 1] = (coef[i + 1] + l_rate * error * yhat * (1.0 - yhat) * row[i])
-		print('>epoch=%d, lrate=%.3f, error=%.3f, b=%.3f, w=%.3f' % (epoch, l_rate, sum_error, coef[0], coef[i+1]))
-	return coef
+				coeff[i + 1] = (coeff[i + 1] + LR * error * yhat * (1.0 - yhat) * row[i])
+		print('>epoch=%d, lrate=%.3f, error=%.3f, b=%.3f, w=%.3f' % (epoch, LR, summError, coeff[0], coeff[i+1]))
+	return coeff
 
 
-def logLinearReg(train, test, LR, epochs):
+def runLogisticRegression(train, test, LR, epochs):
+    #  the performance of the logReg.
     preds = list()
-    coef = coefficients_sgd(train, LR, epochs)
+    coeff = coefficientsSGD(train, LR, epochs)
     for row in test:
-        yHat = predict(row, coef)
+        yHat = predict(row, coeff)
         yHat = round(yHat)
         preds.append(yHat)
     return preds
@@ -86,31 +95,16 @@ def evalAlgo(dataset, algo, *args):
 
     return accuracy
 
-
-# Calculate coefficients
-# dataset = getDataSet()
-# l_rate = 0.3
-# n_epoch = 40000
-# coef = coefficients_sgd(dataset, l_rate, n_epoch)
-# print(coef)
-def get_avg_from_arr(arr):
-    sum = 0
-    N = len(arr)
-    for elem in arr:
-        sum+=elem
-    return (sum/N)
-
 LR = 0.17
 epochs = 8000
-# scores = evalAlgo(getDataSet(), logLinearReg, LR, epochs)
+
 print("\n\n\n")
 # print(f"Final Accuracy: {scores}")
 
 runs = []
 
-
 for run in range(5):
-    score = evalAlgo(getDataSet(), logLinearReg, LR, epochs)
+    score = evalAlgo(getDataSet(), runLogisticRegression, LR, epochs)
     runs.append(score)
     
 print(f'''\n\n
